d4e3527d41846c87cff5def66f5a5a36cf5883de
Blazej <bwielk@gmail.com>
2017-01-13 00:28:26 +0000

    Develop the card_game project

--- Hand.java
+++ Hand.java
@@ -1,3 +1,11 @@
+import java.util.*;
+package card_game;
+
 public class Hand{
+
+  private ArrayList<Card>hand = new ArrayList<Card>();
   
+  public Hand(){
+    this.hand = new ArrayList<Card>();
+  }
 }
--- Player.java
+++ Player.java
@@ -1,3 +1,10 @@
+package card_game;
+
 public class Player{
   
+  private String name;
+
+  public Player(String name){
+    this.name = name;
+  }
 }
